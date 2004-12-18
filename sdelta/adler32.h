6aa1b76b25c1de9d3618a48c43e524c85c520820
svinn <svinn>
2004-12-18 16:53:33 +0000

    fixed endian detection on FreeBSD/bigendian

--- sdelta/adler32.h
+++ sdelta/adler32.h
@@ -1,21 +1,31 @@
-/* $Id: adler32.h,v 1.3 2004/12/12 21:07:12 svinn Exp $ */
+/* $Id: adler32.h,v 1.4 2004/12/18 16:53:33 svinn Exp $ */
 
 #include <sys/types.h>
+
 #if !defined(__FreeBSD__) && !defined(sun) && !defined(__sun)
 #include <endian.h>
-#endif
+#endif /* !__FreeBSD__ && !sun && !__sun */
 
 #if defined(sun) || defined(__sun)
 typedef uint32_t u_int32_t;
 typedef uint16_t u_int16_t;
-#endif
+#if !defined(BYTE_ORDER) && !defined(__BYTE_ORDER)
+#define LITTLE_ENDIAN 1234
+#define BIG_ENDIAN 4321
+#ifdef _LITTLE_ENDIAN
+#define BYTE_ORDER LITTLE_ENDIAN
+#else
+#define BYTE_ORDER BIG_ENDIAN
+#endif /* _LITTLE_ENDIAN */
+#endif /* BYTE_ORDER */
+#endif /* sun || __sun */
 
 typedef union DWORD {
 
   u_int32_t  dword;
 
   struct {
-    #if __BYTE_ORDER == __LITTLE_ENDIAN || BYTE_ORDER == LITTLE_ENDIAN || defined(_LITTLE_ENDIAN)
+    #if __BYTE_ORDER == __LITTLE_ENDIAN || BYTE_ORDER == LITTLE_ENDIAN
       u_int16_t  low;
       u_int16_t  high;
     #else 
@@ -25,7 +35,7 @@ typedef union DWORD {
   }  word;
 
   struct {
-    #if __BYTE_ORDER == __LITTLE_ENDIAN || BYTE_ORDER == LITTLE_ENDIAN || defined(_LITTLE_ENDIAN)
+    #if __BYTE_ORDER == __LITTLE_ENDIAN || BYTE_ORDER == LITTLE_ENDIAN
       unsigned char  b0;
       unsigned char  b1;
       unsigned char  b2;
