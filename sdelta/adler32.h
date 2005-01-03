ffbd00ccfd2bf61752d41aeb2daa0b5b2bc7d122
svinn <svinn>
2005-01-03 22:10:10 +0000

    a lot of endian fixes

--- sdelta/adler32.h
+++ sdelta/adler32.h
@@ -1,56 +1,73 @@
-/* $Id: adler32.h,v 1.6 2004/12/29 22:48:39 svinn Exp $ */
+/* $Id: adler32.h,v 1.7 2005/01/03 22:10:10 svinn Exp $ */
 
 #include <sys/types.h>
-
-#if !defined(__FreeBSD__) && !defined(sun) && !defined(__sun)
-#include <endian.h>
-#endif /* !__FreeBSD__ && !sun && !__sun */
 
 #if defined(sun) || defined(__sun)
 typedef uint32_t u_int32_t;
 typedef uint16_t u_int16_t;
-#if !defined(BYTE_ORDER) && !defined(__BYTE_ORDER)
-#define LITTLE_ENDIAN 1234
-#define BIG_ENDIAN 4321
+#ifndef __BYTE_ORDER
+#define __LITTLE_ENDIAN	1234
+#define __BIG_ENDIAN	4321
 #ifdef _LITTLE_ENDIAN
-#define BYTE_ORDER LITTLE_ENDIAN
+#define __BYTE_ORDER	__LITTLE_ENDIAN
 #else
-#define BYTE_ORDER BIG_ENDIAN
+#define __BYTE_ORDER	__BIG_ENDIAN
 #endif /* _LITTLE_ENDIAN */
-#endif /* BYTE_ORDER */
+#endif /* __BYTE_ORDER */
 #endif /* sun || __sun */
+
+
+#ifdef __linux__
+#include <endian.h>
+#elif defined(_BYTE_ORDER) && !defined(__BYTE_ORDER)
+/* FreeBSD-5 NetBSD DFBSD TrustedBSD */
+#define __BYTE_ORDER	_BYTE_ORDER
+#define __LITTLE_ENDIAN	_LITTLE_ENDIAN
+#define __BIG_ENDIAN	_BIG_ENDIAN
+#define __PDP_ENDIAN	_PDP_ENDIAN
+#elif defined(BYTE_ORDER) && !defined(__BYTE_ORDER)
+/* FreeBSD-[34] OpenBSD MirBSD */
+#define __BYTE_ORDER	BYTE_ORDER
+#define __LITTLE_ENDIAN	LITTLE_ENDIAN
+#define __BIG_ENDIAN	BIG_ENDIAN
+#define __PDP_ENDIAN	PDP_ENDIAN
+#endif
+
+#ifndef __BYTE_ORDER
+#error __BYTE_ORDER is undefined
+#endif
 
 typedef union DWORD {
 
   u_int32_t  dword;
 
   struct {
-    #if __BYTE_ORDER == __LITTLE_ENDIAN || BYTE_ORDER == LITTLE_ENDIAN
+    #if __BYTE_ORDER == __LITTLE_ENDIAN
       u_int16_t  low;
       u_int16_t  high;
-    #else 
+    #else
       u_int16_t  high;
       u_int16_t  low;
     #endif
   }  word;
 
   struct {
-    #if __BYTE_ORDER == __LITTLE_ENDIAN || BYTE_ORDER == LITTLE_ENDIAN
+    #if __BYTE_ORDER == __LITTLE_ENDIAN
       unsigned char  b0;
       unsigned char  b1;
       unsigned char  b2;
       unsigned char  b3;
-    #elif  __BYTE_ORDER == __PDP_ENDIAN || BYTE_ORDER == PDP_ENDIAN
-      unsigned char  b2;
-      unsigned char  b3;
-      unsigned char  b1;
-      unsigned char  b0;
-    #else  /*  __BYTE_ORDER == _BIG_ENDIAN  */
+    #elif __BYTE_ORDER == __BIG_ENDIAN
       unsigned char  b3;
       unsigned char  b2;
       unsigned char  b1;
       unsigned char  b0;
-    #endif  
+    #else /* __BYTE_ORDER == __PDP_ENDIAN */
+      unsigned char  b2;
+      unsigned char  b3;
+      unsigned char  b1;
+      unsigned char  b0;
+    #endif
   }  byte;
 
 } DWORD;
