2d5d0c94b21df1de9843b5eea6ff6ced7715207f
svinn <svinn>
2005-01-10 00:16:22 +0000

    MAP_NORESERVE and mremap support

--- sdelta/input.c
+++ sdelta/input.c
@@ -1,155 +1,170 @@
-/* $Id: input.c,v 1.3 2005/01/09 00:16:02 svinn Exp $ */
+/* $Id: input.c,v 1.4 2005/01/10 00:16:22 svinn Exp $ */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include "input.h"
 
+
 void load_buf(const char *fname, INPUT_BUF *b) {
-	struct stat st;
-	size_t alloc_size = 0, cnt;
+    struct stat st;
+    size_t alloc_size = 0, cnt;
 #if defined(USE_MADVISE) && defined(USE_MAP_ANON_STDIN)
-	size_t offset;	/* to cut off the excess */
+    size_t offset;	/* to cut off the excess */
 #endif
 
-	if (fname) {
-		if (stat(fname, &st) == -1) {
-			perror(fname);
-			exit(EXIT_FAILURE);
-		}
+    if (fname) {
+	if (stat(fname, &st) == -1) {
+	    perror(fname);
+	    exit(EXIT_FAILURE);
+	}
+	/* on FreeBSD, open(dirname, O_RDONLY) succeeds */
+	if (S_ISDIR(st.st_mode)) {
+	    fprintf(stderr, "%s: is a directory\n", fname);
+	    exit(EXIT_FAILURE);
+	}
 
-		if ((b->fd = open(fname, O_RDONLY)) == -1) {
-			perror(fname);
-			exit(EXIT_FAILURE);
-		}
+	if ((b->fd = open(fname, O_RDONLY)) == -1) {
+	    perror(fname);
+	    exit(EXIT_FAILURE);
+	}
 
-		/* regular file or a symlink, so it is safe to use st.st_size */
-		if (S_ISREG(st.st_mode) || S_ISLNK(st.st_mode)) {
-			alloc_size = st.st_size;
+	/* regular file or a symlink, so it is safe to use st.st_size */
+	if (S_ISREG(st.st_mode)) {
+	    alloc_size = st.st_size;
 #ifdef USE_MMAP
-			/* let's try to mmap it directly */
-			if ((b->buf = mmap(NULL, b->mmap_size = b->size = alloc_size, PROT_READ, MAP_PRIVATE, b->fd, 0)) == MAP_FAILED)
-				/* mmap failed: try to use malloc */
-				fprintf(stderr, "%s: mmap(size = %u, fd = %d) failed\n", fname, alloc_size, b->fd);
-			else {
-				/* mmaped successfully, so return */
-				b->is_mmaped = 1;
-				MADVISE(b->buf, alloc_size, MADV_SEQUENTIAL);
-				return;
-			}
-#endif
-		}
-		/* if it is a device, or USE_MMAP is not defined, or mmap failed,
-		   use malloc then */
-	}
-	else {
-		/* fname is NULL, so it must be stdin */
-		b->fd = 0;
-#ifdef USE_MAP_ANON_STDIN
-		/* try to prepare MAP_ANON, if available */
-		if ((b->buf = mmap(NULL, alloc_size = b->mmap_size = MAX_MAP_ANON, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)) == MAP_FAILED)
-			/* mmap failed, let's try to use malloc */
-			fprintf(stderr, "warning: stdin: mmap(size = %u, MAP_ANON) failed\n", MAX_MAP_ANON);
-		else
-			b->is_mmaped = 1;
+	    /* let's try to mmap it directly */
+	    if ((b->buf = mmap(NULL, b->mmap_size = b->size = alloc_size, PROT_READ, MAP_PRIVATE, b->fd, 0)) == MAP_FAILED)
+		/* mmap failed: try to use malloc */
+		fprintf(stderr, "warning: %s: mmap(size = %u, fd = %d) failed\n", fname, alloc_size, b->fd);
+	    else {
+		/* mmaped successfully, so return */
+		b->is_mmaped = 1;
+		MADVISE(b->buf, alloc_size, MADV_SEQUENTIAL);
+		return;
+	    }
 #endif
 	}
-
-	/* from this point, no mmaps can happen, and the only mmaped
-	   memory we can encounter is MAP_ANON (that is, stdin) */
-
-	/* use malloc */
+	/* if it is a device, or USE_MMAP is not defined, or mmap failed,
+	   use malloc then */
+    }
+    else {
+	/* fname is NULL, so it must be stdin */
+	b->fd = 0;
 #ifdef USE_MAP_ANON_STDIN
-	if (! b->is_mmaped) {
+	/* try to prepare MAP_ANON, if available */
+	if ((b->buf = mmap(NULL, alloc_size = b->mmap_size = MAX_MAP_ANON, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE | MAP_NORESERVE, -1, 0)) == MAP_FAILED)
+	    /* mmap failed, let's try to use malloc */
+	    fprintf(stderr, "warning: stdin: mmap(size = %u, MAP_ANON) failed\n", MAX_MAP_ANON);
+	else
+	    b->is_mmaped = 1;
 #endif
-		/* if we know the exact size, add 1 byte */
-		alloc_size = alloc_size ? alloc_size + 1 : IBUF_ALLOC_QUANT;
-		if (!(b->buf = malloc(alloc_size))) {
-			fprintf(stderr, "%s: malloc(%u) failed\n", fname ? fname : "stdin", alloc_size);
-			exit(EXIT_FAILURE);
-		}
+    }
+
+    /* from this point, no mmaps can happen, and the only mmaped
+       memory we can encounter is MAP_ANON (that is, stdin) */
+
+    /* use malloc */
 #ifdef USE_MAP_ANON_STDIN
+    if (! b->is_mmaped) {
+#endif
+	/* if we know the exact size, add 1 byte */
+	alloc_size = alloc_size ? alloc_size + 1 : IBUF_ALLOC_QUANT;
+	if (!(b->buf = malloc(alloc_size))) {
+	    fprintf(stderr, "%s: malloc(%u) failed\n", fname ? fname : "stdin", alloc_size);
+	    exit(EXIT_FAILURE);
 	}
-#endif
-
-	while (cnt = read(b->fd, b->buf + b->size, alloc_size - b->size)) {
-		if (cnt == -1) {
-			perror("read");
-			exit(EXIT_FAILURE);
-		}
-
-		b->size += cnt;
-
-		/* input buffer is full: fatal error, if it is a MAP_ANON */
-		if (alloc_size == b->size) {
 #ifdef USE_MAP_ANON_STDIN
-			if (! b->fd && b->is_mmaped) {
-				fprintf(stderr, "stdin: exceeded MAP_ANON size (%u)\n", alloc_size);
-				exit(EXIT_FAILURE);
-			}
-			else
+    }
 #endif
-			/* try to realloc */
-			if (!(b->buf = realloc(b->buf, alloc_size += IBUF_ALLOC_QUANT))) {
-				fprintf(stderr, "load_buf: realloc(buf, %u) failed\n", alloc_size);
-				exit(EXIT_FAILURE);
-			}
-		}
+
+    while (cnt = read(b->fd, b->buf + b->size, alloc_size - b->size)) {
+	if (cnt == -1) {
+	    perror("read");
+	    exit(EXIT_FAILURE);
 	}
 
+	b->size += cnt;
+
+	/* input buffer is full: fatal error, if it is a MAP_ANON */
+	if (alloc_size == b->size) {
 #ifdef USE_MAP_ANON_STDIN
-	/* not mmaped, ergo, malloc'ed */
-	if (! b->is_mmaped) {
+	    if (b->is_mmaped) {
+		fprintf(stderr, "stdin: exceeded MAP_ANON size (%u)\n", alloc_size);
+		exit(EXIT_FAILURE);
+	    }
+	    else
 #endif
-		/* try to realloc it if needed */
-		if (alloc_size - b->size > 1 && !(b->buf = realloc(b->buf, b->size))) {
-			fprintf(stderr, "realloc(buf, %u) failed\n", b->size);
-			exit(EXIT_FAILURE);
-		}
-#ifdef USE_MAP_ANON_STDIN
+	    /* try to realloc */
+	    if (!(b->buf = realloc(b->buf, alloc_size += IBUF_ALLOC_QUANT))) {
+		fprintf(stderr, "load_buf: realloc(buf, %u) failed\n", alloc_size);
+		exit(EXIT_FAILURE);
+	    }
 	}
+    }
+
+#ifdef USE_MAP_ANON_STDIN
+    /* not mmaped, ergo, malloc'ed */
+    if (! b->is_mmaped) {
+#endif
+	/* try to realloc it if needed */
+	if (alloc_size - b->size > 1 && !(b->buf = realloc(b->buf, b->size))) {
+	    fprintf(stderr, "realloc(buf, %u) failed\n", b->size);
+	    exit(EXIT_FAILURE);
+	}
+#ifdef USE_MAP_ANON_STDIN
+    }
+#endif
+
+#if defined(USE_MAP_ANON_STDIN) && defined(USE_MREMAP)
+    /* if mremap is available, use it */
+    if (b->is_mmaped &&
+	    (b->buf = mremap(b->buf, MAX_MAP_ANON, b->mmap_size = b->size, MREMAP_MAYMOVE)) == MAP_FAILED) {
+	fputs("mremap failed\n", stderr);
+	exit(EXIT_FAILURE);
+    }
 #endif
 
 #if defined(USE_MAP_ANON_STDIN) && defined(USE_MADVISE)
-	/* use MADVISE to cut off the excess */
-	if (! b->fd && b->is_mmaped) {
-		if ((offset = b->size) % PAGE_SIZE)
-			offset += PAGE_SIZE - offset % PAGE_SIZE;
-		if (alloc_size > offset)
-			MADVISE(b->buf + offset, alloc_size - offset, MADV_DONTNEED);
-
-		MADVISE(b->buf, offset, MADV_SEQUENTIAL);
-	}
+    if (b->is_mmaped) {
+#ifndef USE_MREMAP
+	/* if mremap is unavailable, use MADVISE to cut off the excess */
+	if ((offset = b->size) % PAGE_SIZE)
+	    offset += PAGE_SIZE - offset % PAGE_SIZE;
+	if (alloc_size > offset)
+	    MADVISE(b->buf + offset, alloc_size - offset, MADV_DONTNEED);
+#endif
+	/* one final madvise */
+	MADVISE(b->buf, b->size, MADV_SEQUENTIAL);
+    }
 #endif
 
-	close(b->fd);
-	b->fd = -1; /* unload_buf will know that it is closed */
+    close(b->fd);
+    b->fd = -1; /* unload_buf will know that it is closed */
 }
 
 
 void unload_buf(INPUT_BUF *b) {
-	if (! b->buf)
-		return;
+    if (! b->buf)
+	return;
 
 #if defined(USE_MMAP) || defined(USE_MAP_ANON_STDIN)
-	if (b->is_mmaped) {
-		if (b->mmap_size && munmap(b->buf, b->mmap_size) == -1)
-			fprintf(stderr, "warning: munmap(0x%p, 0x%08X) failed\n", b->buf, b->mmap_size);
-		b->buf = NULL;
-		/* close if it is not closed */
-		if (b->fd != -1) {
-			close(b->fd);
-			b->fd = -1;
-		}
-		return;
-	}
-#endif
-	/* regular malloc, so just free() it */
-	free(b->buf);
+    if (b->is_mmaped) {
+	if (b->mmap_size && munmap(b->buf, b->mmap_size) == -1)
+	    fprintf(stderr, "warning: munmap(0x%p, 0x%08X) failed\n", b->buf, b->mmap_size);
 	b->buf = NULL;
+	/* close if it is not closed */
+	if (b->fd != -1) {
+	    close(b->fd);
+	    b->fd = -1;
+	}
+	return;
+    }
+#endif
+    /* regular malloc, so just free() it */
+    free(b->buf);
+    b->buf = NULL;
 }
 
-/* vi: set ts=4 sw=4: */
+/* vi: set ts=8 sw=4: */
--- sdelta/input.h
+++ sdelta/input.h
@@ -1,50 +1,52 @@
-/* $Id: input.h,v 1.1 2005/01/08 22:01:47 svinn Exp $ */
+/* $Id: input.h,v 1.2 2005/01/10 00:16:23 svinn Exp $ */
 
 #ifndef __INPUT_H__
 #define __INPUT_H__
 
+
 #include <sys/types.h>
+#include <sys/stat.h>
 #ifdef __linux__
 #include <sys/user.h>
 #else
 #include <sys/param.h>
 #endif
 #include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 /* one realloc step */
-#define IBUF_ALLOC_QUANT 0x400000
+#define IBUF_ALLOC_QUANT	0x400000
 
 /* mmap is enabled by default */
 #define USE_MMAP
 
-
 #if defined(__linux__) || defined(__FreeBSD__)
 #define USE_MADVISE
-#endif
-
-#ifdef __FreeBSD__
 #define USE_MAP_ANON_STDIN
-#define MAX_MAP_ANON 0x40000000
+#define MAX_MAP_ANON		0x40000000
 #endif
 
+#ifdef __linux__
+#define USE_MREMAP
+#endif
 
 #ifdef USE_MADVISE
-
 /* USE_MADVISE implies USE_MMAP */
 #ifndef USE_MMAP
 #define USE_MMAP
 #endif
-
 /* MADVISE_IBUF macro needs this INPUT_BUF pointer to know
    whether the buffer was mmaped */
 #define MADVISE_IBUF(ibuf, addr, len, behav) do { \
-	if (ibuf->is_mmaped && madvise(addr, len, behav) == -1) \
-		fprintf(stderr, "warning: madvise(0x%p, 0x%08X, %d) failed\n", addr, len, behav); \
+    if (ibuf->is_mmaped && madvise(addr, len, behav) == -1) \
+	fprintf(stderr, "warning: madvise(0x%p, 0x%08X, %d) failed\n", addr, len, behav); \
 } while(0)
 #define MADVISE(addr, len, behav) do { \
-	if (madvise(addr, len, behav) == -1) \
-		fprintf(stderr, "warning: madvise(0x%p, 0x%08X, %d) failed\n", addr, len, behav); \
+    if (madvise(addr, len, behav) == -1) \
+	fprintf(stderr, "warning: madvise(0x%p, 0x%08X, %d) failed\n", addr, len, behav); \
 } while(0)
 #else
 /* do not use madvise */
@@ -53,11 +55,11 @@
 #endif /* USE_MADVISE */
 
 typedef struct {
-	int fd;
-	unsigned char *buf;
-	size_t size;
-	size_t mmap_size; /* `mmap_size' can be greater than `size' */
-	int is_mmaped;
+    int fd;
+    unsigned char *buf;
+    size_t size;
+    size_t mmap_size; /* `mmap_size' can be greater than `size' */
+    int is_mmaped;
 } INPUT_BUF;
 
 void load_buf(const char *, INPUT_BUF *);
@@ -65,4 +67,4 @@ void unload_buf(INPUT_BUF *);
 
 #endif /* __INPUT_H__ */
 
-/* vi: set ts=4 sw=4: */
+/* vi: set ts=8 sw=4: */
