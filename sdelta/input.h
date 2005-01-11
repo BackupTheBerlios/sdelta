64d21237e4f2ec810be9052c5393637f3bd15e63
svinn <svinn>
2005-01-11 17:49:31 +0000

    improvements

--- sdelta/input.h
+++ sdelta/input.h
@@ -1,21 +1,29 @@
-/* $Id: input.h,v 1.2 2005/01/10 00:16:23 svinn Exp $ */
+/* $Id: input.h,v 1.3 2005/01/11 17:49:31 svinn Exp $ */
 
 #ifndef __INPUT_H__
 #define __INPUT_H__
 
-
 #include <sys/types.h>
 #include <sys/stat.h>
+
+/* PAGE_SIZE definition */
 #ifdef __linux__
 #include <sys/user.h>
+#elif defined(__NetBSD__)
+#include <sys/vmparam.h>
 #else
 #include <sys/param.h>
+#if (defined(sun) || defined(__sun)) && !defined(PAGE_SIZE)
+#define PAGE_SIZE PAGESIZE
 #endif
+#endif
+
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 
 /* one realloc step */
 #define IBUF_ALLOC_QUANT	0x400000
@@ -23,25 +31,27 @@
 /* mmap is enabled by default */
 #define USE_MMAP
 
-#if defined(__linux__) || defined(__FreeBSD__)
-#define USE_MADVISE
-#define USE_MAP_ANON_STDIN
-#define MAX_MAP_ANON		0x40000000
-#endif
-
+/* if USE_MAP_ANON_STDIN is undefined, mremap is not used */
 #ifdef __linux__
 #define USE_MREMAP
 #endif
 
-#ifdef USE_MADVISE
-/* USE_MADVISE implies USE_MMAP */
-#ifndef USE_MMAP
-#define USE_MMAP
+#if defined(MAP_ANON) && defined(MAP_NORESERVE)
+#define USE_MAP_ANON_STDIN
+#define MAX_MAP_ANON		0x40000000
 #endif
+
+/* to disable mmap, MAP_ANON and madvise, uncomment the following: */
+/* #undef USE_MMAP
+#undef USE_MAP_ANON_STDIN */
+
+/* to disable *madvise only*, change the following line to `#if 0<newline>' */
+#if defined(MADV_SEQUENTIAL) && defined(MADV_FREE) && (defined(USE_MMAP) || defined(USE_MAP_ANON_STDIN))
+#define USE_MADVISE
 /* MADVISE_IBUF macro needs this INPUT_BUF pointer to know
    whether the buffer was mmaped */
 #define MADVISE_IBUF(ibuf, addr, len, behav) do { \
-    if (ibuf->is_mmaped && madvise(addr, len, behav) == -1) \
+    if (ibuf->mmap_size && madvise(addr, len, behav) == -1) \
 	fprintf(stderr, "warning: madvise(0x%p, 0x%08X, %d) failed\n", addr, len, behav); \
 } while(0)
 #define MADVISE(addr, len, behav) do { \
@@ -52,14 +62,15 @@
 /* do not use madvise */
 #define MADVISE_IBUF(ibuf, addr, len, behav)
 #define MADVISE(addr, len, behav)
-#endif /* USE_MADVISE */
+#endif
 
 typedef struct {
     int fd;
     unsigned char *buf;
     size_t size;
-    size_t mmap_size; /* `mmap_size' can be greater than `size' */
-    int is_mmaped;
+#if defined(USE_MMAP) || defined(USE_MAP_ANON_STDIN)
+    size_t mmap_size;
+#endif
 } INPUT_BUF;
 
 void load_buf(const char *, INPUT_BUF *);
